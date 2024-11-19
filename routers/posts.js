60ffabfd2082585a1cdd919244cf835d2ee92e35
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 17:29:54 +0100

    export crude ES Modules

--- routers/posts.js
+++ routers/posts.js
@@ -1,8 +1,46 @@
-const express = require("express"); //importiamo express framework
+import express from "express";; //importiamo express framework
 const router = express.Router(); //invochiamo funzione Router
+
+//STEP 4 - CRUDE E ROTTA PRINCIPALE
+
+//index
+router.get("/", (req, res) => {
+  console.log("Index pasticini");
+  res.send("Index pasticini");
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Detagli del pasticino ${id}`);
+  res.send(`Detagli del pasticino ${id}`);
+});
+
+//store
+router.post("/", (req, res) => {
+  console.log("Creiamo un nuovo pasticino");
+  res.send("Creiamo un nuovo pasticino");
+});
+
+//update
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Aggiornamento pasticino ${id}`);
+  res.send(`Aggiornamento pasticino ${id}`);
+});
+
+//modify
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Aggiornamento parziale pasticino ${id}`);
+  res.send(`Aggiornamento parziale pasticino ${id}`);
+});
+
+//deleted
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Eliminazione pasticino ${id}`);
+  res.send(`Eliminazione  pasticino ${id}`);
+});
 
 
 export { router }; //sportiamo router
-
-
-
