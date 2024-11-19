ec2695ebf561a0a714e1e70c0821b31ff87cbd72
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 15:59:55 +0100

    define main root

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 console.log("Hello");
 
+//STEP 1 import and init
 import express from "express";
 import { posts } from "./direct.js"; // import direct.js  ES Modules
 const app = express(); // salviamo la istanza principale
 const port = 666; // # porta
 
+//STEP 3 - rotta principale
+app.get("/", (re, res) => {
+  console.log("Server del mio blog");
+  res.send("Benvenuto al server del mio blog");
+});
 
-
-
-
-
-
-
+//STEP 2  - server ascolta client
 app.listen(port, () => {
-  //server ascolta client
   console.log(`server in ascolto http://localhost:${port}`);
 });
