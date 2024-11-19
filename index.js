9fe5b41f4d4326957a65277534f567f6bf750fb3
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 15:54:39 +0100

    add coments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Hello");
 
 import express from "express";
-import { posts } from "./direct.js"; // import ES Modules
+import { posts } from "./direct.js"; // import direct.js  ES Modules
 
 const app = express(); // salviamo la istanza principale
 const port = 666; // # porta
