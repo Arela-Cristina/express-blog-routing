97f91a367e9cfec1744aa9a18a7c3073c810505a
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 15:54:07 +0100

    init express instance and set a port number

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Hello");
 
-import express from "express"; 
+import express from "express";
 import { posts } from "./direct.js"; // import ES Modules
 
+const app = express(); // salviamo la istanza principale
+const port = 666; // # porta
