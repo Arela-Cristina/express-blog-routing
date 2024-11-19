0d25de69372c58e02f332a234562a5c8b547d355
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 15:57:18 +0100

    server listen to client

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ console.log("Hello");
 
 import express from "express";
 import { posts } from "./direct.js"; // import direct.js  ES Modules
-
 const app = express(); // salviamo la istanza principale
 const port = 666; // # porta
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  //server ascolta client
+  console.log(`server in ascolto http://localhost:${port}`);
+});
