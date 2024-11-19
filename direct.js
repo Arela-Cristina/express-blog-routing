75406dbfa208fd0a75a2febb8d960d910eaf20d7
Arela-Cristina <instagrandine@gmail.com>
2024-11-19 15:39:30 +0100

    add data structure

---
+++ direct.js
@@ -0,0 +1,29 @@
+const posts = [
+  {
+    title: "Post 1",
+    img: "/images/ciambellone.jpeg",
+    id: 1,
+  },
+  {
+    title: "Post 2",
+    img: "/images/cracker_barbabietola.jpeg",
+    id: 2,
+  },
+  {
+    title: "Post 3",
+    img: "/images/pane_fritto_dolce.jpeg",
+    id: 3,
+  },
+  {
+    title: "Post 4",
+    img: "/images/pasta_barbabietola.jpeg",
+    id: 4,
+  },
+  {
+    title: "Post 5",
+    img: "/images/torta_paesana.jpeg",
+    id: 5,
+  },
+];
+
+export { posts }; //export to index.js ES Modules
